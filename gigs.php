1d101364cae28eeae598bc013e55c0d5760762e9
Alastair Appleton <awa21@cam.ac.uk>
2019-12-12 17:50:21 +0000

    Updated gigs

--- gigs.php
+++ gigs.php
@@ -3,195 +3,210 @@
 
 <head>
 
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <title>The Alley Cats: Gigs</title>
-  <link rel="icon" href="images/paw.svg" type="image/png" />
-  <!-- Milligram and dependencies-->
-  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic|Miltonian" />
-  <link rel="stylesheet" href="normalize.css" />
-  <link rel="stylesheet" href="milligram.min.css" />
-  <!-- Custom code-->
-  <link rel="stylesheet" href="alleycats.css" />
-  <!-- Fancybox -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
-  <!--Font Awesome -->
-  <link href="fonts/fontawesome/css/all.min.css" rel="stylesheet" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>The Alley Cats: Gigs</title>
+    <link rel="icon" href="images/paw.svg" type="image/png" />
+    <!-- Milligram and dependencies-->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic|Miltonian" />
+    <link rel="stylesheet" href="normalize.css" />
+    <link rel="stylesheet" href="milligram.min.css" />
+    <!-- Custom code-->
+    <link rel="stylesheet" href="alleycats.css" />
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
+    <!--Font Awesome -->
+    <link href="fonts/fontawesome/css/all.min.css" rel="stylesheet" />
 
 </head>
 
 <body>
 
-  <div id="background-container">
-    <div id="stars"></div>
-    <div id="clouds"></div>
-  </div> <!-- end background container -->
+    <div id="background-container">
+        <div id="stars"></div>
+        <div id="clouds"></div>
+    </div> <!-- end background container -->
 
-  <div class="container">
+    <div class="container">
 
-    <?php include 'nav.php';?>
+        <?php include 'nav.php'; ?>
 
-    <main id="content" class="row transition-fade">
+        <main id="content" class="row transition-fade">
 
 
-      <div class="column ">
-        
-       <section id="gigs">
-          <h2>Gigs</h2>
-          <p>
-            We perform in and around Cambridge, at a number of family events, local festivals and jazz venues. We also
-            do community-based and charitable work with groups including the East Anglia Children's Hospice. Here are
-            details of some of our upcoming and
-            previous performances.
-          </p>
+            <div class="column ">
 
-          <h3>Upcoming gigs</h3>
-          <table>
-            <tbody>
-              <tr>
-                <td>6 December 2019</td>
-                <td>
-                  Winter Lights festival at <a href="https://www.nationaltrust.org.uk/anglesey-abbey-gardens-and-lode-mill/features/anglesey-abbey-winter-lights">Anglesey Abbey</a>, Cambridge.
-                </td>
-              </tr>
+                <section id="gigs">
+                    <h2>Gigs</h2>
+                    <p>
+                        We perform in and around Cambridge, at a number of family events, local festivals and jazz
+                        venues. We also
+                        do community-based and charitable work with groups including the East Anglia Children's Hospice.
+                        Here are
+                        details of some of our upcoming and
+                        previous performances.
+                    </p>
+
+                    <h3>Upcoming gigs</h3>
+                    <table>
+                        <tbody>
+
                             <tr>
-                <td>8 December 2019</td>
-                <td>
-                  Our second performance at the Winter Lights festival, <a href="https://www.nationaltrust.org.uk/anglesey-abbey-gardens-and-lode-mill/features/anglesey-abbey-winter-lights">Anglesey Abbey</a>, Cambridge.
-                </td>
-              </tr>
+                                <td>15 December 2019</td>
+                                <td>
+                                    Come and check out our final performance at the Winter Lights festival, <a
+                                        href="https://www.nationaltrust.org.uk/anglesey-abbey-gardens-and-lode-mill/features/anglesey-abbey-winter-lights">Anglesey
+                                        Abbey</a>, Cambridge.
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="divider"><img src="images/dividers/divider.png" alt="" /></div>
+
+                    <h3>Previous gigs</h3>
+                    <table>
+                        <tbody>
+                            <td>8 December 2019</td>
+                            <td>
+                                Our second performance at the Winter Lights festival, <a
+                                    href="https://www.nationaltrust.org.uk/anglesey-abbey-gardens-and-lode-mill/features/anglesey-abbey-winter-lights">Anglesey
+                                    Abbey</a>, Cambridge.
+                            </td>
+                            </tr>
                             <tr>
-                <td>15 December 2019</td>
-                <td>
-                  Come and check out our final performance at the Winter Lights festival, <a href="https://www.nationaltrust.org.uk/anglesey-abbey-gardens-and-lode-mill/features/anglesey-abbey-winter-lights">Anglesey Abbey</a>, Cambridge.
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                                <td>6 December 2019</td>
+                                <td>
+                                    Winter Lights festival at <a
+                                        href="https://www.nationaltrust.org.uk/anglesey-abbey-gardens-and-lode-mill/features/anglesey-abbey-winter-lights">Anglesey
+                                        Abbey</a>, Cambridge.
+                                </td>
+                            </tr>
+                            <tr>
 
-          <div class="divider"><img src="images/dividers/divider.png" alt="" /></div>
-
-          <h3>Previous gigs</h3>
-          <table>
-            <tbody>
-              <tr>
-                <td>16 November 2019</td>
-                <td>
-                  The Alley Cats <strong>with special guest Dennis Rollins</strong> at <a
-                    href="https://www.cambridgejazzfestival.info/the-alley-cats"><em>
-                      Cambridge International Jazz
-                      Festival</em></a>. Storey's Field Centre, Cambridge.<br />
-                  Live painting workshop with Gina Southgate at <em><a
-                      href="https://www.facebook.com/events/399743357341209">Cambridge International Jazz
-                      Festival</a></em>. Storey's Field Centre, Cambridge.
-                </td>
-              </tr>
-              <tr>
-                <td>August 2019</td>
-                <td><a href="https://www.cambridgelive.org.uk/city-events/events/mayors-day-out-0"><em>
-                      Cambridge City Council Mayor's Day
-                      Out
-                    </em></a>, Great Yarmouth.</td>
-              </tr>
-              <tr>
-                <td>August 2019</td>
-                <td>Private function. Wissington, Suffolk. </td>
-              </tr>
-              <tr>
-                <td>June 2019</td>
-                <td>Hot Numbers caf&eacute;, Cambridge.</td>
-              </tr>
-              <tr>
-                <td>June 2019</td>
-                <td>The <em>Wild Wood Rumpus</em> family festival, Linton.</td>
-              </tr>
-              <tr>
-                <td>November 2018</td>
-                <td><em>Cambridge International Jazz Festival</em>. Storey's Field Centre, Cambridge.</td>
-              </tr>
-              <tr>
-                <td>September 2018</td>
-                <td>The Alley Cats with special guest Alan Barnes. <em>Jazz and Brass in the Parks</em>, Cambridge.</td>
-              </tr>
-              <tr>
-                <td>June 2018</td>
-                <td><em>F&ecirc;te de la Musique</em>, Saffron Walden.</td>
-              </tr>
-              <tr>
-                <td>April 2018</td>
-                <td>Hot Numbers caf&eacute;, Cambridge.</td>
-              </tr>
-              <tr>
-                <td>September 2017</td>
-                <td>Charity fire walk and fundraising event for East Anglia Children's Hospice, Milton.</td>
-              </tr>
-              <tr>
-                <td>September 2017</td>
-                <td>Private function. Cambridge.</td>
-              </tr>
-              <tr>
-                <td>August 2017</td>
-                <td><em>EACHFEST</em>, East Anglia Children’s Hospice, Milton.</td>
-              </tr>
-              <tr>
-                <td>July 2017</td>
-                <td><em>WillowFest</em> family festival. The Willow Tree, Bourn.</td>
-              </tr>
-              <tr>
-                <td>May 2017</td>
-                <td>The Willow Tree, Bourn.</td>
-              </tr>
-              <tr>
-                <td>March 2017</td>
-                <td>Fundraising event for East Anglia Children's Hospice. Hot Numbers caf&eacute;, Cambridge.</td>
-              </tr>
-              <tr>
-                <td>August 2016</td>
-                <td><em>EACHFEST</em>, East Anglia Children’s Hospice, Milton.</td>
-              </tr>
-
-
-            </tbody>
-          </table>
-
-        </section>
-      
-      </div>
-      <!--end column-->
-    </main>
-    <!--end content row-->
-
-        <?php include 'footer.php';?>
-    <!-- end footer -->
-  </div>
-  <!--end container-->
-
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+                            <tr>
+                                <td>16 November 2019</td>
+                                <td>
+                                    The Alley Cats <strong>with special guest Dennis Rollins</strong> at <a
+                                        href="https://www.cambridgejazzfestival.info/the-alley-cats"><em>
+                                            Cambridge International Jazz
+                                            Festival</em></a>. Storey's Field Centre, Cambridge.<br />
+                                    Live painting workshop with Gina Southgate at <em><a
+                                            href="https://www.facebook.com/events/399743357341209">Cambridge
+                                            International Jazz
+                                            Festival</a></em>. Storey's Field Centre, Cambridge.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>August 2019</td>
+                                <td><a href="https://www.cambridgelive.org.uk/city-events/events/mayors-day-out-0"><em>
+                                            Cambridge City Council Mayor's Day
+                                            Out
+                                        </em></a>, Great Yarmouth.</td>
+                            </tr>
+                            <tr>
+                                <td>August 2019</td>
+                                <td>Private function. Wissington, Suffolk. </td>
+                            </tr>
+                            <tr>
+                                <td>June 2019</td>
+                                <td>Hot Numbers caf&eacute;, Cambridge.</td>
+                            </tr>
+                            <tr>
+                                <td>June 2019</td>
+                                <td>The <em>Wild Wood Rumpus</em> family festival, Linton.</td>
+                            </tr>
+                            <tr>
+                                <td>November 2018</td>
+                                <td><em>Cambridge International Jazz Festival</em>. Storey's Field Centre, Cambridge.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>September 2018</td>
+                                <td>The Alley Cats with special guest Alan Barnes. <em>Jazz and Brass in the Parks</em>,
+                                    Cambridge.</td>
+                            </tr>
+                            <tr>
+                                <td>June 2018</td>
+                                <td><em>F&ecirc;te de la Musique</em>, Saffron Walden.</td>
+                            </tr>
+                            <tr>
+                                <td>April 2018</td>
+                                <td>Hot Numbers caf&eacute;, Cambridge.</td>
+                            </tr>
+                            <tr>
+                                <td>September 2017</td>
+                                <td>Charity fire walk and fundraising event for East Anglia Children's Hospice, Milton.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>September 2017</td>
+                                <td>Private function. Cambridge.</td>
+                            </tr>
+                            <tr>
+                                <td>August 2017</td>
+                                <td><em>EACHFEST</em>, East Anglia Children’s Hospice, Milton.</td>
+                            </tr>
+                            <tr>
+                                <td>July 2017</td>
+                                <td><em>WillowFest</em> family festival. The Willow Tree, Bourn.</td>
+                            </tr>
+                            <tr>
+                                <td>May 2017</td>
+                                <td>The Willow Tree, Bourn.</td>
+                            </tr>
+                            <tr>
+                                <td>March 2017</td>
+                                <td>Fundraising event for East Anglia Children's Hospice. Hot Numbers caf&eacute;,
+                                    Cambridge.</td>
+                            </tr>
+                            <tr>
+                                <td>August 2016</td>
+                                <td><em>EACHFEST</em>, East Anglia Children’s Hospice, Milton.</td>
+                            </tr>
 
 
+                        </tbody>
+                    </table>
 
-  <script>
+                </section>
+
+            </div>
+            <!--end column-->
+        </main>
+        <!--end content row-->
+
+        <?php include 'footer.php'; ?>
+        <!-- end footer -->
+    </div>
+    <!--end container-->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+
+
+
+    <script>
     $('[data-fancybox]').fancybox({
-      buttons: [
-        "zoom",
-        "close",
-      ],
-      loop: true,
+        buttons: [
+            "zoom",
+            "close",
+        ],
+        loop: true,
     });
-  </script>
+    </script>
 
-  <?php
-     // require_once('/public/home/awa21/public_html/alleycats/owa/owa_php.php');
-     // $owa = new owa_php();
-     // $owa->setSiteId('b7a9cffaec5fcd59b41880c3be77209d');
-     // $owa->setPageTitle('Gigs');
-     // $owa->trackPageView();
-    ?>
+    <?php
+  // require_once('/public/home/awa21/public_html/alleycats/owa/owa_php.php');
+  // $owa = new owa_php();
+  // $owa->setSiteId('b7a9cffaec5fcd59b41880c3be77209d');
+  // $owa->setPageTitle('Gigs');
+  // $owa->trackPageView();
+  ?>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/pjax@latest/pjax.min.js"></script>  
-  <script src="alleycats-pjax.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/pjax@latest/pjax.min.js"></script>
+    <script src="alleycats-pjax.js"></script>
 
 
 
