22e946712de27b0fa9ef2d6ba515ebb5bebfd452
alastairappleton <alastair.appleton+github@gmail.com>
2020-02-28 22:24:36 +0000

    Added new videos

--- media.php
+++ media.php
@@ -29,41 +29,47 @@
 
   <div class="container">
 
-    <?php include 'nav.php';?>
+    <?php include 'nav.php'; ?>
 
     <main id="content" class="row transition-fade">
 
 
       <div class="column ">
-        
-           <section id="media">
+
+        <section id="media">
           <h2>Media</h2>
           <h3>Watch</h3>
           <p>We recently recorded a new video at <a href="http://saltwellstudio.co.uk/">Saltwell Studio</a>, showcasing
             some of our best material. Check it out below, and give us a "like" on YouTube!</p>
 
           <div id="showreel">
-            <iframe width="100%" height="400" src="https://www.youtube.com/embed/YhxU9_Cjo2Y" frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="400" src="https://www.youtube.com/embed/YhxU9_Cjo2Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
 
-          <div class="divider"><img src="images/dividers/divider.png" alt="" /></div>
+<div class="row">
+          <div id="bewitching-jeannie" class="column">
+            <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/uhQW-e9CDKU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+
+          <div id="i-wanna-be-like-you" class="column">
+            <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/X3SbhulvMa4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div>
+          
+</div>
+
+<div class="divider"><img src="images/dividers/divider.png" alt="" /></div>
 
           <h3>Listen</h3>
           <div class="row soundcloud">
             <div id="i-wanna-be-like-you" class="column">
-              <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661309070&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+              <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661309070&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
             </div>
 
             <div id="under-the-sea" class="column">
-              <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661308227&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+              <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661308227&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
             </div>
 
             <div id="when-i-see-an-elephant-fly" class="column">
-              <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661310891&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+              <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661310891&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
             </div>
           </div>
 
@@ -73,35 +79,29 @@
 
           <div class="gallery">
 
-            <a href="images/gallery/gallery001.jpg" data-fancybox="gallery"
-              data-caption="(Credit: Dominikas &Zcaron;alys)">
+            <a href="images/gallery/gallery001.jpg" data-fancybox="gallery" data-caption="(Credit: Dominikas &Zcaron;alys)">
               <img src="images/gallery/thumb001.jpg" alt="Mask and feathers" />
             </a>
 
-            <a href="images/gallery/gallery003.jpg" data-fancybox="gallery"
-              data-caption="Ros Russell (Credit: Dominikas &Zcaron;alys)">
+            <a href="images/gallery/gallery003.jpg" data-fancybox="gallery" data-caption="Ros Russell (Credit: Dominikas &Zcaron;alys)">
               <img src="images/gallery/thumb003.jpg" alt="Ros Russell" />
             </a>
 
-            <a href="images/gallery/gallery004.jpg" data-fancybox="gallery"
-              data-caption="The Alley Cats at Hot Numbers (Credit: Dominikas &Zcaron;alys)">
+            <a href="images/gallery/gallery004.jpg" data-fancybox="gallery" data-caption="The Alley Cats at Hot Numbers (Credit: Dominikas &Zcaron;alys)">
               <img src="images/gallery/thumb004.jpg" alt="The Alley Cats at Hot Numbers" />
             </a>
 
             <div class="gallery-item">
-              <a href="images/gallery/gallery005.jpg" data-fancybox="gallery"
-                data-caption="(Credit: Dominikas &Zcaron;alys)">
+              <a href="images/gallery/gallery005.jpg" data-fancybox="gallery" data-caption="(Credit: Dominikas &Zcaron;alys)">
                 <img src="images/gallery/thumb005.jpg" alt="Playing drums at Hot Numbers" />
               </a>
             </div>
 
-            <a href="images/gallery/gallery006.jpg" data-fancybox="gallery"
-              data-caption="(Credit: Dominikas &Zcaron;alys)">
+            <a href="images/gallery/gallery006.jpg" data-fancybox="gallery" data-caption="(Credit: Dominikas &Zcaron;alys)">
               <img src="images/gallery/thumb006.jpg" alt="Alley Cats sign and balloon" />
             </a>
 
-            <a href="images/gallery/gallery008.jpg" data-fancybox="gallery"
-              data-caption="(Credit: Dominikas &Zcaron;alys)">
+            <a href="images/gallery/gallery008.jpg" data-fancybox="gallery" data-caption="(Credit: Dominikas &Zcaron;alys)">
               <img src="images/gallery/thumb008.jpg" alt="Colouring in" />
             </a>
 
@@ -109,13 +109,11 @@
               <img src="images/gallery/thumb009.jpg" alt="Stripy socks and shoes with red bows" />
             </a>
 
-            <a href="images/gallery/gallery010.jpg" data-fancybox="gallery"
-              data-caption="Paul Stubbs (Credit: Trevor Lee)">
+            <a href="images/gallery/gallery010.jpg" data-fancybox="gallery" data-caption="Paul Stubbs (Credit: Trevor Lee)">
               <img src="images/gallery/thumb010.jpg" alt="Paul Stubbs playing trumpet" />
             </a>
 
-            <a href="images/gallery/gallery011.jpg" data-fancybox="gallery"
-              data-caption="Ros Russell (Credit: Trevor Lee)">
+            <a href="images/gallery/gallery011.jpg" data-fancybox="gallery" data-caption="Ros Russell (Credit: Trevor Lee)">
               <img src="images/gallery/thumb011.jpg" alt="Ros Russell singing" />
             </a>
 
@@ -123,23 +121,19 @@
               <img src="images/gallery/thumb012.jpg" alt="Stripy monkey toy" />
             </a>
 
-            <a href="images/gallery/gallery013.jpg" data-fancybox="gallery"
-              data-caption="Giulio Lampronti at Saltwell Studio">
+            <a href="images/gallery/gallery013.jpg" data-fancybox="gallery" data-caption="Giulio Lampronti at Saltwell Studio">
               <img src="images/gallery/thumb013.jpg" alt="Giulio Lampronti playing bass" />
             </a>
 
-            <a href="images/gallery/gallery014.jpg" data-fancybox="gallery"
-              data-caption="Joe Taylor at Saltwell Studio">
+            <a href="images/gallery/gallery014.jpg" data-fancybox="gallery" data-caption="Joe Taylor at Saltwell Studio">
               <img src="images/gallery/thumb014.jpg" alt="Joe Taylor playing drums" />
             </a>
 
-            <a href="images/gallery/gallery015.jpg" data-fancybox="gallery"
-              data-caption="The Alley Cats at Saltwell Studio">
+            <a href="images/gallery/gallery015.jpg" data-fancybox="gallery" data-caption="The Alley Cats at Saltwell Studio">
               <img src="images/gallery/thumb015.jpg" alt="The Alley Cats at Saltwell Studio" />
             </a>
 
-            <a href="images/gallery/gallery016.jpg" data-fancybox="gallery"
-              data-caption="Simon Fothergill at Saltwell Studio">
+            <a href="images/gallery/gallery016.jpg" data-fancybox="gallery" data-caption="Simon Fothergill at Saltwell Studio">
               <img src="images/gallery/thumb016.jpg" alt="Simon Fothergill at Saltwell Studio" />
             </a>
 
@@ -151,27 +145,26 @@
               <img src="images/gallery/thumb018.jpg" alt="Jane Hollingworth at the Britannia Theatre" />
             </a>
 
-            <a href="images/gallery/gallery019.jpg" data-fancybox="gallery"
-              data-caption="Britannia Theatre, Great Yarmouth">
+            <a href="images/gallery/gallery019.jpg" data-fancybox="gallery" data-caption="Britannia Theatre, Great Yarmouth">
               <img src="images/gallery/thumb019.jpg" alt="The stage at the Britannia Theatre" />
             </a>
 
           </div>
         </section>
 
-      
+
       </div>
       <!--end column-->
     </main>
     <!--end content row-->
 
-    <?php include 'footer.php';?>
+    <?php include 'footer.php'; ?>
 
   </div>
   <!--end container-->
 
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
 
   <script>
     $('[data-fancybox]').fancybox({
@@ -184,15 +177,15 @@
   </script>
 
   <?php
-     // require_once('/public/home/awa21/public_html/alleycats/owa/owa_php.php');
-     // $owa = new owa_php();
-     // $owa->setSiteId('b7a9cffaec5fcd59b41880c3be77209d');
-     // $owa->setPageTitle('Media');
-     // $owa->trackPageView();
-    ?>
+  // require_once('/public/home/awa21/public_html/alleycats/owa/owa_php.php');
+  // $owa = new owa_php();
+  // $owa->setSiteId('b7a9cffaec5fcd59b41880c3be77209d');
+  // $owa->setPageTitle('Media');
+  // $owa->trackPageView();
+  ?>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/pjax@latest/pjax.min.js"></script>  
+  <script src="https://cdn.jsdelivr.net/npm/pjax@latest/pjax.min.js"></script>
   <script src="alleycats-pjax.js"></script>
 
 
